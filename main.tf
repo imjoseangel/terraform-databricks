73c0140ac30b8fdd5a0fcc70be2bdd35a867c2f6
imjoseangel <josea.munoz@gmail.com>
2022-02-17 23:24:18 +0100

    add secret

--- main.tf
+++ main.tf
@@ -5,7 +5,13 @@ data "external" "env" {
   program = ["jq", "-n", "env"]
 }
 
-resource "databricks_secret_scope" "terraform" {
+resource "databricks_secret_scope" "main" {
   name                     = "application"
   initial_manage_principal = "users"
+}
+
+resource "databricks_secret" "main" {
+  key          = "service_principal_key"
+  string_value = data.external.env.ARM_CLIENT_SECRET
+  scope        = databricks_secret_scope.main.name
 }
