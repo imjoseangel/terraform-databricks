7a37a5fbeecd939133356f6b1ea1eb6af3c1666e
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2022-02-24 16:46:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,28 +5,37 @@ data "external" "env" {
   program = ["jq", "-n", "env"]
 }
 
-resource "databricks_secret_scope" "main" {
-  name                     = "application"
-  initial_manage_principal = "users"
-}
-
-resource "databricks_secret" "main" {
-  key          = "service_principal_key"
-  string_value = data.external.env.result.ARM_CLIENT_SECRET
-  scope        = databricks_secret_scope.main.name
-}
-
+#-------------------------------
+# DB Data Source Declarations
+#-------------------------------
 data "databricks_node_type" "main" {
   local_disk = true
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
 }
 
 data "databricks_spark_version" "main" {
   long_term_support = true
+
+  depends_on = [
+    azurerm_databricks_workspace.main,
+    databricks_secret.main
+  ]
 }
 
 data "databricks_group" "main" {
   display_name = "admins"
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
 }
+
+#-------------------------------
+# DB Data Resource Declarations
+#-------------------------------
 
 resource "databricks_cluster" "main" {
   cluster_name            = "main"
@@ -40,12 +49,20 @@ resource "databricks_cluster" "main" {
   }
 
   is_pinned = true
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
 }
 
 resource "databricks_workspace_conf" "main" {
   custom_config = {
     "enableIpAccessLists" : true
   }
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
 }
 
 resource "databricks_ip_access_list" "main" {
@@ -55,5 +72,32 @@ resource "databricks_ip_access_list" "main" {
     "1.2.3.0/24",
     "1.2.5.0/24"
   ]
-  depends_on = [databricks_workspace_conf.main]
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
 }
+
+#-------------------------------
+# Databricks Secrets
+#-------------------------------
+
+resource "databricks_secret_scope" "main" {
+  name                     = "application"
+  initial_manage_principal = "users"
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
+}
+
+resource "databricks_secret" "main" {
+  key          = "service_principal_key"
+  string_value = data.external.env.result.ARM_CLIENT_SECRET
+  scope        = databricks_secret_scope.main.name
+
+  depends_on = [
+    azurerm_databricks_workspace.main
+  ]
+}
+
